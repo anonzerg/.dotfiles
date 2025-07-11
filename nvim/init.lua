f62e9ed1c619d8eb011b027aa0173c57833a5c8b
zerg <anonzerg@proton.me>
2025-07-12 00:30:19 +0330

    lsp and some quality changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,7 @@ vim.opt.wildignorecase = true
 vim.opt.smartcase = true
 vim.opt.incsearch = true
 vim.opt.wrapscan = false
-vim.opt.wrap = false
---vim.opt.breakindent = true
---vim.opt.linebreak = true
+vim.opt.wrap = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -26,17 +24,17 @@ vim.opt.colorcolumn = "80"
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.mouse = ""
---vim.opt.scrolloff = 0
+-- vim.opt.scrolloff = 0
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    local last_line = vim.fn.getline('$')
-    if last_line ~= '' then
-      vim.api.nvim_buf_set_lines(0, -1, -1, false, {""})
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*",
+--   callback = function()
+--     local last_line = vim.fn.getline('$')
+--     if last_line ~= '' then
+--       vim.api.nvim_buf_set_lines(0, -1, -1, false, {""})
+--     end
+--   end,
+-- })
 
 local function jump_to_last_pos()
   local last_pos = vim.api.nvim_buf_get_mark(0, '"')
@@ -68,49 +66,77 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 local lspconfig = require("lspconfig")
 
 local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(
+    bufnr,
+    "omnifunc",
+    "v:lua.vim.lsp.omnifunc"
+  )
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("LspFormatting", {
+        clear = true
+      }),
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({
+          async = false
+        })
+      end,
+    })
+  end
   local opts = {
     noremap = true,
   }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(
+    bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts
+  )
 end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 lspconfig.gopls.setup({
   on_attach = on_attach,
+  capabilities = capabilities,
 })
 lspconfig.clangd.setup({
   on_attach = on_attach,
+  capabilities = capabilities,
+})
+lspconfig.zls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
 })
 
 vim.diagnostic.config({
-  update_in_insert = true,
-  virtual_text = true,
+  update_in_insert = false,
+  virtual_lines = true,
+  -- virtual_text = true,
   signs = false,
   underline = true,
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(env)
-    local client = vim.lsp.get_client_by_id(env.data.client_id)
-    if client.supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, env.buf, {autotrigger = true})
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("CursorHold", {
+--   callback = function()
+--     vim.diagnostic.open_float(nil, {
+--       focusable = false,
+--       border = "single",
+--       max_width = vim.o.columns,
+--       wrap = true,
+--       severity_sort = true,
+--     })
+--   end,
+-- })
+
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   callback = function(env)
+--     local client = vim.lsp.get_client_by_id(env.data.client_id)
+--     if client.supports_method("textDocument/completion") then
+--       vim.lsp.completion.enable(true, client.id, env.buf, {autotrigger = true})
+--     end
+--   end,
+-- })
 
 vim.o.updatetime = 128
-
-vim.api.nvim_create_autocmd("CursorHold", {
-  callback = function()
-    vim.diagnostic.open_float(nil, {
-      focusable = false,
-      border = "single",
-      max_width = 80,
-      wrap = true,
-      severity_sort = true,
-    })
-  end,
-})
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("HighlightYank", {clear = true}),
@@ -124,20 +150,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 require("nvim-treesitter.configs").setup {
-  ensure_installed = {"c", "zig", "lua", "python", "rust", "ruby", "go", "make", "html", "css", "javascript"},
+  ensure_installed = {
+    "c", "zig", "lua", "python", "rust", "ruby", "go", "make", "html", "css",
+    "javascript",
+  },
   auto_install = false,
 }
 
 vim.o.cursorline = true
 vim.api.nvim_set_hl(0, "CursorLine", {fg = "NONE"})
-vim.api.nvim_set_hl(0, "CursorLineNr", {fg = "#FCE094"})
+vim.api.nvim_set_hl(0, "CursorLineNr", {fg = "#fce094"})
 
 vim.o.statusline = " %F %h%m%r%=%-14.(%l,%c%V%) %y %P "
-
-local opts = {noremap = true, silent = true}
-vim.api.nvim_set_keymap("i", "(", "()<Left>", opts)
-vim.api.nvim_set_keymap("i", "[", "[]<Left>", opts)
-vim.api.nvim_set_keymap("i", "{", "{}<Left>", opts)
-vim.api.nvim_set_keymap("i", "\"", "\"\"<Left>", opts)
-vim.api.nvim_set_keymap("i", "'", "''<Left>", opts)
 
